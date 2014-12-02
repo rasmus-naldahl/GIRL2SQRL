dfbbb66d0b1018577d5a3394b3fba0b8b5f36ea8
Matt <mtoothman@users.noreply.github.com>
2014-12-02 15:53:17 -0500

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,15 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	//v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	//v = v.replace(/\bThe cloud\b/g, "My butt");
-	//v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	//v = v.replace(/\bthe cloud\b/g, "my butt");
-	
 	v = v.replace(/\bGirl\b/g, "Squirrel");
-	//v = v.replace(/\bgirl\b/g, "squirrel");
-	//v = v.replace(/\bGirls\b/g, "Squirrels");
-	//v = v.replace(/\bgirls\b/g, "squirrels");
+	v = v.replace(/\bgirl\b/g, "squirrel");
+	v = v.replace(/\bGirls\b/g, "Squirrels");
+	v = v.replace(/\bgirls\b/g, "squirrels");
 	
 	textNode.nodeValue = v;
 }
